85f77143503010271d786aff3ddf3a29b9c2e20a
tamtam <tam.duong@ascenx.com>
2018-05-15 16:24:54 +0000

    1st stable

--- daiphun.sh
+++ daiphun.sh
@@ -1,7 +1,11 @@
-#!/bin/bash
-cd /home/pi/WorkSpace/daiphun
+#exec 3>&1 4>&2
+#trap 'exec 2>&4 1>&3' 0 1 2 3
+#exec 1> /home/orange/Log.out 2>&1
+
+cd /home/orange/backup/daiphunBinary
  export LD_LIBRARY_PATH=`pwd`/lib
  export QT_QPA_PLATFORM_PLUGIN_PATH=`pwd`/lib/plugins/platforms
  export QT_PLUGIN_PATH=`pwd`/lib/plugins
+ export PATH=$PATH:`pwd`/lib
 #export QT_IM_MODULE=qtvirtualkeyboard
-`pwd`/testQTCPserver
+`pwd`/testQTCPserver &
